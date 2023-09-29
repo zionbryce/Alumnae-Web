603086e7f2e7eb93496fb4ebc1d58e0f7f114bb4
zionbryce <zionbryce2001@gmail.com>
2023-09-29 15:08:32 +0800

    9/29/2023 second commit
    Navbar update

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import './App.css';
 import {LogIn} from './components/SuperAdmin/LogIn/LogIn';
 import { Searchdash } from './components/SuperAdmin/Searchdash/Searchdash';
+import { Navigation } from './components/SuperAdmin/navigation/navigation';
+import { Home } from './components/SuperAdmin/home/home';
 
 export const App = () => {
   return (
     <BrowserRouter>
     <div>
+      <Navigation>
       <Routes>
 
       <Route path='/' element={<LogIn/>}/>
       <Route path='/search' element={<Searchdash/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/home' element={<Home/>}/>
       </Routes>
-
+      </Navigation>
     </div>
     </BrowserRouter>
   )
